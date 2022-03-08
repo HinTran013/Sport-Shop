0a351ecc5c796ad0568511dec364d5c4f580a395
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2022-03-08 13:24:24 +0700

    built bottom navigation bar structure

---
+++ screens/ShopScreen.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ShopScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>This is shop screen</Text>
+    </View>
+  );
+}
---
+++ src/features/Navigation/MainContainer.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+//screens
+import HomeScreen from "../../../screens/HomeScreen";
+import ShopScreen from "../../../screens/ShopScreen";
+import CartScreen from "../../../screens/CartScreen";
+import FavoritesScreen from "../../../screens/FavoritesScreen";
+import ProfileScreen from "../../../screens/ProfileScreen";
+
+//screen names
+const homeName = "Home";
+const shopName = "Shop";
+const cartName = "Cart";
+const favoritesName = "Favorites";
+const profileName = "Profile";
+
+const Tab = createBottomTabNavigator();
+
+export default function MainContainer() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? "home" : "home-outline";
+            } else if (rn === shopName) {
+              iconName = focused ? "pricetags" : "pricetags-outline";
+            } else if (rn === cartName) {
+              iconName = focused ? "cart" : "cart-outline";
+            } else if (rn === favoritesName) {
+              iconName = focused ? "heart" : "heart-outline";
+            } else if (rn === profileName) {
+              iconName = focused ? "person" : "person-outline";
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={shopName} component={ShopScreen} />
+        <Tab.Screen name={cartName} component={CartScreen} />
+        <Tab.Screen name={favoritesName} component={FavoritesScreen} />
+        <Tab.Screen name={profileName} component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
