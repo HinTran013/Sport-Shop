8a189f8187eafde72e825e3af4bacbc305847314
Khai Hoan <khaihoan2306@gmail.com>
2022-03-10 14:24:34 +0700

    Complete Sign Up Screen

--- App.js
+++ App.js
@@ -2,14 +2,13 @@ import React from "react";
 import {
   SafeAreaView
 } from "react-native";
-import LoginScreen from "./screens/LoginScreen";
 import MainContainer from "./src/features/Navigation/MainContainer";
 import GlobalStyles from './src/GlobalStyles';
 
 function App() {
   return(
     <SafeAreaView style={GlobalStyles.droidSafeArea}>
-      <LoginScreen />
+      <MainContainer />
     </SafeAreaView>
   );
 }
