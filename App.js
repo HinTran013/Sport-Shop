86e1055a18619b36396af717d385c23b2864c3a4
Khai Hoan <khaihoan2306@gmail.com>
2022-03-11 14:56:00 +0700

    Complete Order section

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from "react";
 import {
   SafeAreaView
 } from "react-native";
+import OrderDetailScreen from "./screens/OrderDetailScreen";
 import MainContainer from "./src/features/Navigation/MainContainer";
 import GlobalStyles from './src/GlobalStyles';
 
 function App() {
   return(
     <SafeAreaView style={GlobalStyles.droidSafeArea}>
-      <MainContainer />
+      <OrderDetailScreen />
     </SafeAreaView>
   );
 }
