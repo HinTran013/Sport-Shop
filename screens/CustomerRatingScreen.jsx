db4cecdb5683ccf3437279390563f16ab04a0912
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2022-04-01 17:59:22 +0700

    done rating page

--- screens/CustomerRatingScreen.jsx
+++ screens/CustomerRatingScreen.jsx
@@ -3,30 +3,66 @@ import { View, Text, StyleSheet, ScrollView } from "react-native";
 import SimpleScreenHeader from "../src/components/Simple Screen Header/SimpleScreenHeader";
 import ProductRating from "../src/components/Product Rating/ProductRating";
 import ReviewCard from "../src/components/Product Rating/ReviewCard";
+import { Button } from "react-native-elements";
+import { Icon } from "react-native-elements";
 
 const CustomerRatingScreen = ({ navigation }) => {
   return (
-    <ScrollView style={styles.container}>
-      <SimpleScreenHeader
-        headerTitle="Rating and Review"
-        onBackPress={() => navigation.pop()}
+    <View style={{ position: "relative", flex: 1 }}>
+      <ScrollView style={styles.container}>
+        <SimpleScreenHeader
+          headerTitle="Rating and Review"
+          onBackPress={() => navigation.pop()}
+        />
+        <View style={styles.mainContentContainer}>
+          <View style={{ alignItems: "center", marginTop: 15 }}>
+            <ProductRating />
+          </View>
+
+          <View>
+            <Text style={{ fontWeight: "bold", fontSize: 24, marginTop: 15 }}>
+              8 Reviews
+            </Text>
+          </View>
+
+          <View>
+            <ReviewCard containerStyle={{ marginTop: 25 }} />
+            <ReviewCard containerStyle={{ marginTop: 25 }} />
+            <ReviewCard containerStyle={{ marginTop: 25 }} />
+            <ReviewCard containerStyle={{ marginTop: 25 }} />
+          </View>
+        </View>
+
+        <View style={{ paddingBottom: 80 }}></View>
+      </ScrollView>
+      <Button
+        iconPosition="left"
+        icon={
+          <Icon
+            name="create"
+            size={15}
+            color="white"
+            style={{ marginRight: 5 }}
+          />
+        }
+        title={"Write a review"}
+        containerStyle={{
+          position: "absolute",
+          right: 12,
+          bottom: 70,
+          elevation: 10,
+          borderRadius: 999,
+        }}
+        buttonStyle={{
+          borderRadius: 999,
+          paddingLeft: 15,
+          paddingRight: 15,
+          paddingTop: 10,
+          paddingBottom: 10,
+          backgroundColor: "#DB3022",
+        }}
       />
-      <View style={styles.mainContentContainer}>
-        <View style={{ alignItems: "center", marginTop: 15 }}>
-          <ProductRating />
-        </View>
-
-        <View>
-          <Text style={{ fontWeight: "bold", fontSize: 24, marginTop: 15 }}>
-            8 Reviews
-          </Text>
-        </View>
-
-        <View style={{ marginTop: 15 }}>
-          <ReviewCard />
-        </View>
-      </View>
-    </ScrollView>
+    </View>
   );
 };
 
@@ -35,8 +71,8 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   mainContentContainer: {
-    paddingLeft: 20,
-    paddingRight: 20,
+    paddingLeft: 25,
+    paddingRight: 25,
     flex: 1,
   },
 });
